bbdecd5e6b38765b99b3609aaa4754f0cecd33a4
Jeff Boek <jeff@jeffboek.com>
2016-10-04 14:11:05 -0700

    adds tests for - Splitting into paragraphs - Headers h1 and h2 blocks

--- src/MiloDuo/MiloDuo.fs
+++ src/MiloDuo/MiloDuo.fs
@@ -1,4 +1,26 @@
 ﻿module MiloDuo
 
-let foo a b =
-    a + b
+open System
+open System.Text.RegularExpressions
+
+type Header =
+    | H1 of string
+    | H2 of string
+    | H3 of string
+    | H4 of string
+    | H5 of string
+    | H6 of string
+
+type Paragraph =
+    | Header of Header
+    | Normal of string
+
+let parseParagraph (input: string) =
+    match input |> Seq.toList with
+    | '#'::'#'::rest -> rest.ToString() |> H2 |> Header
+    | '#'::rest -> rest.ToString().Trim() |> H1 |> Header
+    | _ -> Normal input
+
+let parse (input: string) =
+    let paragraphs = Regex.Split(input.Trim(), "\n\n")
+    paragraphs |> Seq.map parseParagraph
--- test/MiloDuo.Tests/Tests.fs
+++ test/MiloDuo.Tests/Tests.fs
@@ -3,9 +3,42 @@
 open Xunit
 open MiloDuo
 
+let testData = """
+# Hello World
+
+## Paragraph 2
+
+This is my markdown file!
+"""
+
 [<Fact>]
-let ``MiloDuo adds 5 and 3``() =
-    let expected = 8
-    let actual = foo 5 3
+let ``Test that it can split a file into paragraphs``() =
+    let expected = 3
+    let actual = parse(testData) |> Seq.length
+
     Assert.Equal(expected, actual)
 
+[<Fact>]
+let ``Test that it detects the first block is an h1``() =
+    let actual = parse(testData) |> Seq.tryHead
+    let isHeader = actual
+                    |> Option.bind (fun p -> match p with
+                                                | Header header -> Some header
+                                                | _ -> None)
+                    |> Option.bind (fun h -> match h with | H1 _ -> Some true | _ -> None)
+                    |> Option.isSome
+
+
+    Assert.Equal(isHeader, true)
+
+[<Fact>]
+let ``Test that it detects the second block is an h2``() = 
+    let actual = testData |> parse |> Seq.tryItem 1
+    let isHeader = actual
+                    |> Option.bind (fun p -> match p with
+                                                | Header header -> Some header
+                                                | _ -> None)
+                    |> Option.bind (fun h -> match h with | H2 _ -> Some true | _ -> None)
+                    |> Option.isSome
+
+    Assert.Equal(isHeader, true)
