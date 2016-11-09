1b2afb1f2f89c706b77d8d547e748795d79e0ee7
Jeff Boek <jeff@jeffboek.com>
2016-11-08 16:42:39 -0800

    tokenizer

--- src/MiloDuo/MiloDuo.fs
+++ src/MiloDuo/MiloDuo.fs
@@ -11,91 +11,43 @@ module private Regex =
     let Split p i =
         Regex.Split(i, p)
 
-type Link = string
-type Span =
+module AST =
+    type Link = string
+    type Span =
     | Literal of string
-    | Emphasis of string
-    | Bold of string
-    | Href of Link * string
+    | Emphasis of Span
+    | Bold of Span
+    | Href of Link * Span
 
+    type Header =
+    | H1 of Span list
+    | H2 of Span list
+    | H3 of Span list
+    | H4 of Span list
+    | H5 of Span list
+    | H6 of Span list
 
-let appendCharToString = sprintf "%s%c"
-
-type Result<'T, 'U> =
-    | Success of 'T
-    | Failure of 'U
-
-let parseLiteral (s : string) =
-
-    let matcher (token : string) tokens currentGroup =
-        let headMatchesToken = tokens |> List.tryHead |> Option.map ((=) token)
-        match token with
-        | "__" ->
-            if headMatchesToken = Some(true) then Success (Emphasis currentGroup, tokens |> List.tail)
-            else Failure (List.Cons (token, tokens))
-        | "**" ->
-            if headMatchesToken = Some(true) then Success (Bold currentGroup, tokens |> List.tail)
-            else Failure (List.Cons (token, tokens))
-        | _ -> Failure tokens
-
-    let rec parseStr (spans : Span List) (tokens : string List) (currentGroup : string) remaining =
-        match remaining with
-        | '_'::'_'::rest ->
-            let result = matcher "__" tokens currentGroup
-            match result with
-            | Success(span, tokens) -> parseStr (List.Cons (span, spans)) tokens "" rest
-            | Failure(tokens) -> parseStr spans tokens currentGroup rest
-        | '*'::'*'::rest ->
-            let result = matcher "**" tokens currentGroup
-            match result with
-            | Success(span, tokens) -> parseStr (List.Cons (span, spans)) tokens "" rest
-            | Failure(tokens) -> parseStr spans tokens currentGroup rest
-        | head::tail -> parseStr spans tokens (appendCharToString currentGroup head) tail
-        | [] -> spans
-
-    parseStr [] [] "" (s |> Seq.toList)
-
-type Header =
-    | H1 of Span List
-    | H2 of Span List
-    | H3 of Span List
-    | H4 of Span List
-    | H5 of Span List
-    | H6 of Span List
-
-type Paragraph =
+    type Paragraph =
+    | Paragraph of Span list
     | Header of Header
-    | Basic of Span List
 
-type Document =
-    | Body of Paragraph List
+    type Document = Body of Paragraph list
 
-let private parseParagraph i =
-    match i |> Seq.toList with
-    | '#'::'#'::'#'::'#'::'#'::'#'::rest -> rest.ToString() |> parseLiteral |> H6 |> Header
-    | '#'::'#'::'#'::'#'::'#'::rest -> rest.ToString() |> parseLiteral |> H5 |> Header
-    | '#'::'#'::'#'::'#'::rest -> rest.ToString() |> parseLiteral |> H4 |> Header
-    | '#'::'#'::'#'::rest -> rest.ToString() |> parseLiteral |> H3 |> Header
-    | '#'::'#'::rest -> rest.ToString() |> parseLiteral |> H2 |> Header
-    | '#'::rest -> rest.ToString().Trim() |> parseLiteral |> H1 |> Header
-    | _ -> i |> parseLiteral |> Basic
+    let paragraphs d =
+        match d with
+        | Body paragraphs -> paragraphs
 
-let parse =
-    String.Trim
-    >> Regex.Split "\n\n"
-    >> Seq.map parseParagraph
-    >> Seq.toList
-    >> Body
+module Tokenizer =
+    let tokenize s =
+        "foo"
 
-let tryCastHeader p =
-    match p with
-    | Header header -> Some header
-    | _ -> None
+// module Parser =
+//     open AST
+//     type Progress = { parsed : char list; rest : char list }
 
-let tryHead d =
-    match d with
-    | Body paragraphs -> Seq.tryHead paragraphs
+//     let parse (s : string) =
+//         let parse
 
-let paragraphs d =
-    match d with
-    | Body paragraphs -> paragraphs
+        // Body [
+        //     Header (H1 [Literal "hello world"])
+        // ]
--- test/MiloDuo.Tests/Tests.fs
+++ test/MiloDuo.Tests/Tests.fs
@@ -14,43 +14,6 @@ This is **my** _markdown_ file!
 [<Fact>]
 let ``Test that it can split a file into paragraphs``() =
     let expected = 3
-    let actual = testData |> parse |> paragraphs |> Seq.length
+    let actual = testData |> Parser.parse |> AST.paragraphs |> List.length
 
     Assert.Equal(expected, actual)
-
-[<Fact>]
-let ``Test that it detects the first block is an h1``() =
-    let actual = testData |> parse |> tryHead
-    let isHeader = actual
-                    |> Option.bind tryCastHeader
-                    |> Option.bind (fun h -> match h with | H1 _ -> Some true | _ -> None)
-                    |> Option.isSome
-
-
-    Assert.Equal(isHeader, true)
-
-[<Fact>]
-let ``Test that it detects the second block is an h2``() =
-    let actual = testData |> parse |> paragraphs |> Seq.tryItem 1
-
-    let isHeader = actual
-                    |> Option.bind tryCastHeader
-                    |> Option.bind (fun h -> match h with | H2 _ -> Some true | _ -> None)
-                    |> Option.isSome
-
-    Assert.Equal(isHeader, true)
-
-
-[<Fact>]
-let ``Test that it can parse a span``() =
-    let actual = "__Hello World__" |> parse
-    let expected = Body [ Basic [ Emphasis "Hello World" ] ]
-
-    Assert.Equal(actual, expected)
-
-[<Fact>]
-let ``Test that it can parse a bold``() =
-    let actual = "**Hello World**" |> parse
-    let expected = Body [ Basic [ Bold "Hello World" ] ]
-
-    Assert.Equal(actual, expected)
